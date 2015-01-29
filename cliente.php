cd1eb91764a52cb5c8df42777fa52dbd9086a311
Andrés Bestard <andresbestard@gmail.com>
2015-01-29 22:53:51 +0100

    Implementando UI

--- cliente.php
+++ cliente.php
@@ -5,11 +5,61 @@ $options = array('location' => 'http://piserver.dyndns.org/DAW/DWES/SOAP/taller/
 //create an instante of the SOAPClient (the API will be available)
 $api = new SoapClient(NULL, $options);
 //call an API method
-echo $api->getPVP(1);
-echo $api->getStock(2,2);
-//echo $api->getFamilias();
-//var_dump($api->getFamilias());
 
-//echo $api->getProductosFamilia($cod_familia);
-var_dump($api->getProductosFamilia(2));
+if (isset($_POST["getpvp"]))
+	$pvp = $api->getPVP(1);
+if (isset($_POST["producto"])&&isset($_POST["tienda"]))
+	$stock = $api->getStock($_POST["producto"],$_POST["tienda"]);
+if (isset($_POST["familia"]))
+	$productos = $api->getProductosFamilia($_POST["familia"]);
+$familias = $api->getFamilias();
+
 ?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<style>
+		div{
+			border: solid 2px #006aff;
+			margin: 10px auto;
+			width: 600px;
+			padding: 10px;
+		}
+	</style>
+</head>
+<body>
+<form action="cliente.php" method="POST">
+
+	<div>Núm articulo:<input name="getpvp"type="text" /><?=$pvp."€";?><br /></div>
+
+	<div>Producto:<input name="producto" type="text" />  Tienda:<input name="tienda" type="text" /><?=$stock." unidades en stock";?></div>
+
+	<div>Famílias:<br />
+		<em>
+			<? foreach ($familias as $fam){
+				echo $fam["idfamilias"].". ".$fam["nombre"]."<br>";
+			}?>
+		</em>
+	</div>
+
+
+	<div>Productos familia:<input name="familia" type="text" /><br />
+		<em>
+			<? foreach ($productos as $prod){
+				echo $prod["idproductos"].". ".$prod["nombre"]."<br>";
+			}?>
+		</em>
+	</div>
+
+	<button value="sumbit">Consultar</button>
+
+
+
+
+</form>
+</body>
+</html>
